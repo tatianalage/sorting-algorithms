14560ec19260ab832c8ade495d0425601cdf9cba
Tatiana Lage <105643201+tatianalage@users.noreply.github.com>
2024-05-19 12:28:01 -0300

    Add bubbleSort_Atualizado
    Atualizado com as especificações solicitadas pelo professor!

---
+++ bubbleSort_Atualizado.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <chrono>
+#include <ctime>
+#include <initializer_list>
+#include <fstream>
+#include <cmath> 
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::srand;
+using std::string;
+using std::time;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::nanoseconds;
+using std::ofstream;
+
+typedef struct _node
+{
+    struct _node *ptrNext;
+    struct _node *ptrPrev;
+    int iData;
+} Node;
+
+Node *createNode(int iData);
+void insertNodeEnd(Node **head, int iData);
+void swapData(Node *node1, Node *node2);
+void unoptimizedBubbleSort(Node **head);
+void optimizedBubbleSort(Node **head);
+void printList(Node *head);
+void generateRandomList(Node **head, int size);
+void copyList(Node **head1, Node **head2);
+void isSorted(Node **head);
+
+int main()
+{
+    ofstream outputFile("sorting_times.txt");
+    if (!outputFile.is_open())
+    {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    const int listSize = 10000;
+    const int numLists = 100;
+
+    long long unoptimizedDurations[numLists];
+    long long optimizedDurations[numLists];
+
+    for (int i = 0; i < numLists; i++)
+    {
+        Node *head1 = nullptr;
+        Node *head2 = nullptr;
+
+        generateRandomList(&head1, listSize);
+        copyList(&head1, &head2);
+
+        auto start_unoptimized = high_resolution_clock::now();
+        unoptimizedBubbleSort(&head1);
+        auto stop_unoptimized = high_resolution_clock::now();
+
+        isSorted(&head1);
+
+        auto start_optimized = high_resolution_clock::now();
+        optimizedBubbleSort(&head2);
+        auto stop_optimized = high_resolution_clock::now();
+
+        isSorted(&head2);
+
+        auto duration_unoptimized = duration_cast<nanoseconds>(stop_unoptimized - start_unoptimized).count();
+        auto duration_optimized = duration_cast<nanoseconds>(stop_optimized - start_optimized).count();
+
+        unoptimizedDurations[i] = duration_unoptimized;
+        optimizedDurations[i] = duration_optimized;
+
+        outputFile << "List #" << i + 1 << ": Unoptimized - " << duration_unoptimized << " ns, Optimized - " << duration_optimized << " ns" << endl;
+    }
+    
+    return 0;
+}
+
+Node *createNode(int iData)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->ptrNext = nullptr;
+    newNode->ptrPrev = nullptr;
+    newNode->iData = iData;
+
+    return newNode;
+}
+
+void insertNodeEnd(Node **head, int iData)
+{
+    Node *newNode = createNode(iData);
+    if (*head)
+    {
+        Node *temp = *head;
+        while (temp->ptrNext)
+        {
+            temp = temp->ptrNext;
+        }
+        temp->ptrNext = newNode;
+        newNode->ptrPrev = temp;
+    }
+    else
+    {
+        *head = newNode;
+    }
+}
+
+void swapData(Node *node1, Node *node2)
+{
+    int iTmp = node1->iData;
+    node1->iData = node2->iData;
+    node2->iData = iTmp;
+}
+
+void unoptimizedBubbleSort(Node **head)
+{
+    bool bUnordered = true;
+    while (bUnordered)
+    {
+        bUnordered = false;
+        Node *currNode = *head;
+        while (currNode->ptrNext != nullptr)
+        {
+            if (currNode->iData > currNode->ptrNext->iData)
+            {
+                swapData(currNode, currNode->ptrNext);
+                bUnordered = true;
+            }
+            currNode = currNode->ptrNext;
+        }
+    }
+}
+
+void optimizedBubbleSort(Node **head)
+{
+    Node *lastNode = *head;
+    while (lastNode->ptrNext)
+    {
+        lastNode = lastNode->ptrNext;
+    }
+    bool bUnordered = true;
+    while (bUnordered)
+    {
+        bUnordered = false;
+        Node *currNode = *head;
+        while (currNode != lastNode && currNode != nullptr)
+        {
+            if (currNode->iData > currNode->ptrNext->iData)
+            {
+                swapData(currNode, currNode->ptrNext);
+                bUnordered = true;
+            }
+            currNode = currNode->ptrNext;
+        }
+        lastNode = lastNode->ptrPrev;
+    }
+}
+
+void printList(Node *head)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        cout << temp->iData << " ";
+        temp = temp->ptrNext;
+    }
+    cout << endl;
+}
+
+void generateRandomList(Node **head, int size)
+{
+    srand(time(nullptr));
+    for (int i = 0; i < size; ++i)
+    {
+        int randomNum = rand() % 100;
+        insertNodeEnd(head, randomNum);
+    }
+}
+
+void copyList(Node **head1, Node **head2)
+{
+    if (*head1 == nullptr)
+    {
+        return;
+    }
+    Node *tmp = *head1;
+
+    while (tmp)
+    {
+        insertNodeEnd(head2, tmp->iData);
+        tmp = tmp->ptrNext;
+    }
+}
+
+void isSorted(Node **head)
+{
+    Node *tmp = *head;
+    while (tmp->ptrNext)
+    {
+        if (tmp->iData > tmp->ptrNext->iData)
+        {
+            cout << "NOT SORTED" << endl;
+            exit(1);
+        }
+        tmp = tmp->ptrNext;
+    }
+}
